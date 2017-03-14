6b954e8ea1c1e77f0e5dfac94ea38846be2ca231
Jinal Haria <jinalharia@gmail.com>
2017-03-14 19:31:23 +0000

    build out uber estimates and times

---
+++ config/config.py
@@ -0,0 +1,8 @@
+
+#uber server token
+server_token = "AT8uiEHjg8hx785Dnfo2uR48aD3A7B6G13-5N3rf"
+
+#addresses
+hospital = "Charing Cross Hospital, Fulham Palace Road, London, W6 8RF"
+digby = "14 Digby Mansions, London, W6 9DE"
+
---
+++ utils/uber.py
@@ -0,0 +1,31 @@
+from uber_rides.session import Session
+from uber_rides.client import UberRidesClient
+from utils.geolocation import Address
+import config.config as config
+import pprint
+import pandas as pd
+
+class UberRequests():
+    def __init__(self, start_address, end_address, number_of_people=None):
+        session = Session(server_token=config.server_token)
+        client = UberRidesClient(session)
+        self.start_address = Address(start_address)
+        self.end_address = Address(end_address)
+
+        price_response = client.get_price_estimates(self.start_address.lat, self.start_address.lng, self.end_address.lat, self.end_address.lng,
+                                               seat_count=number_of_people)
+        estimate = price_response.json.get("prices")
+        estimate_df = pd.DataFrame(estimate)
+        estimate_df["duration_mins"] = estimate_df["duration"] / 60.0
+
+        pickup_response = client.get_pickup_time_estimates(start_address.lat, start_address.lng)
+        times = pickup_response.json.get("times")
+        times_df = pd.DataFrame(times)
+        times_df["time_to_pick_up_mins"] = times_df["estimate"] / 60.0
+        times_df = times_df.rename(columns={"estimate": "time_to_pick_up"})
+
+        output = pd.merge(estimate_df, times_df, how="outer", on="display_name")
+        cols = ["display_name", "time_to_pick_up_mins", "distance", "duration_mins", "currency_code", "estimate",
+                "surge_multiplier"]
+        self.output_formatted = output[cols]
+        # pprint.pprint(output_formatted)
